32bfd3ef4c3a3142bfd495597cd97c31c966e2d9
Dina <lutdina04@yandex.ru>
2023-04-14 14:45:18 +1000

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import  AppHeader from './components/header'
+function App() {
+  return (
+    <div>
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AppBar from "material-ui/AppBar"
+export const AppHeader = () => {
+    return (
+        <AppBar
+            title="Title"
+
+        />
+    )
+}
+export default AppHeader;
